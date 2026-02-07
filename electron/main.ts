e6950673cc2344c9fd06fb63f9c04949841a8d48
0neStep <146049978+AperturePlus@users.noreply.github.com>
2026-02-07 19:49:48 +0800

    feat: electron pack

---
+++ electron/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 1280,
+    height: 820,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  const devServerUrl = process.env.VITE_DEV_SERVER_URL;
+  if (devServerUrl) {
+    window.loadURL(devServerUrl);
+  } else {
+    window.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
